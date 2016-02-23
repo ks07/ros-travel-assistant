c796a2bfb7f6da871c1852d7ad731dd501a82184
George Field <gf12815@my.bristol.ac.uk>
2016-02-23 10:29:04 +0000

    Make the action server publish some velocity commands.

--- src/wheely_sim/CrossRoadServer.py
+++ src/wheely_sim/CrossRoadServer.py
@@ -6,14 +6,25 @@ import rospy
 import actionlib
 
 from wheely_sim.msg import CrossRoadAction, CrossRoadResult
+from geometry_msgs.msg import Twist # For simple motor control
 
 class CrossRoadServer:
     def __init__(self):
         self.server = actionlib.SimpleActionServer('cross_road', CrossRoadAction, self.execute, False)
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+        self.cmd_vel_fwd = Twist()
+        self.cmd_vel_fwd.linear.x = 1.0
+
         self.server.start()
 
     def execute(self,goal):
         # TODO: This should do the action
+        rate = rospy.Rate(5.0) # 5 hz
+        for i in range(100): # 20 secs
+            self.pub.publish(self.cmd_vel_fwd)
+            rate.sleep()
+        self.pub.publish(Twist()) # brake
         res = CrossRoadResult()
         res.did_we_make_it = 197
         self.server.set_succeeded(result = res)
