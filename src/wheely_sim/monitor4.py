d9c7ea1ee525efcc1f2e0e0f2f635dfff0fcec83
George Field <gf12815@my.bristol.ac.uk>
2016-04-26 16:16:35 +0100

    Add start of monitor4, before realising it will not work with the timeout.

---
+++ src/wheely_sim/monitor4.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+import os
+import rospy
+import smach
+import smach_ros
+import threading
+from std_msgs.msg import Empty,Int8
+
+testend = False
+
+def log(msg,term = True):
+    global f
+    f.write(str(msg))
+    if term:
+        f.write('\n')
+    else:
+        f.write(',')
+
+class CommandTrigger(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['triggered','inactive','shutdown'],
+                             output_keys=['ct_dest_out'])
+        self.trigger = threading.Event()
+        self.ucmd_sub = rospy.Subscriber('user_commands', Int8, self.ucmd_cb)
+        self.dest = 0
+
+    def ucmd_cb(self, msg):
+        global testend
+        if msg.data == 127:
+            # Shutdown time
+            testend = True
+        else:
+            self.dest = msg.data
+        self.trigger.set()
+
+    def execute(self, userdata):
+        rospy.loginfo('Waiting for command.')
+        triggered = self.trigger.wait(10000)
+        if triggered:
+            if testend:
+                return 'shutdown'
+            log('t',False)
+            userdata.ct_time_out = self.ttime
+            self.trigger.clear()
+            return 'triggered'
+        else:
+            # Add a timeout so that the monitor can be shut off.
+            return 'inactive'
+
+class ResponseChecker(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['passed','failed','shutdown'],
+                             input_keys=['rc_dest_in'],
+                             output_keys=['rc_dest_out'])
+        self.tm1_sub = rospy.Subscriber('test_monitor1', Empty, self.tm1_cb)
+        self.ucmd_sub = rospy.Subscriber('user_commands', Int8, self.ucmd_cb)
+        self.responded = threading.Event()
+    
+    def tm1_cb(self, msg):
+        self.responded.set()
+
+    def ucmd_cb(self, msg):
+        global testend
+        if msg.data == 127:
+            # Shutdown time
+            testend = True
+        self.responded.set()
+        global testend
+        if msg.data == 127:
+            testend = True
+            self.responded.set()
+        
+    def execute(self, userdata):
+        self.responded.clear() # Need to reset the event
+        rospy.loginfo('Waiting for response')
+        triggered = self.responded.wait(30000)
+
+        if testend:
+            return 'shutdown'
+
+        now = time.time()
+        
+        if triggered and time.time() - userdata.rc_time_in < 30.0:
+            log(1)
+            rospy.loginfo('PASS')
+            return 'passed'
+        else:
+            log(0)
+            rospy.loginfo('FAIL')
+            return 'failed'
+
+def main():
+	rospy.init_node('assertion1', anonymous=True)
+
+    	sm = smach.StateMachine(outcomes=['done'])
+
+   	with sm:
+		smach.StateMachine.add('COMMANDTRIGGER', CommandTrigger(), 
+                                       transitions={'triggered':'RESPONSECHECKER',
+                                                    'inactive':'COMMANDTRIGGER',
+                                                    'shutdown':'done'},
+                                       remapping={'ct_dest_out':'dest'})
+		smach.StateMachine.add('RESPONSECHECKER', ResponseChecker(),
+                                       transitions={'passed':'COMMANDTRIGGER',
+                                                    'failed':'COMMANDTRIGGER',
+                                                    'shutdown':'done'},
+                                       remapping={'rc_dest_in':'dest',
+                                                  'rc_dest_out':'dest'})
+
+	# Execute SMACH plan
+    	outcome = sm.execute()
+
+if __name__ == '__main__':
+    if 'MONITORLOG' in os.environ:
+        logfile = os.environ['MONITORLOG']
+    else:
+        logfile = '.monitor1'
+
+    with open(logfile, 'w') as f:
+        try:
+            main()
+        except rospy.ROSInterruptException:
+            pass
