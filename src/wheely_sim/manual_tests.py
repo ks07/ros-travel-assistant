3a7da08d707bb75709f391b132177103fd4c310f
George Field <gf12815@my.bristol.ac.uk>
2016-04-26 09:59:30 +0100

    Add more tests and add script to run directed test suite.

--- src/wheely_sim/manual_tests.py
+++ src/wheely_sim/manual_tests.py
@@ -16,9 +16,15 @@ def runtest(testno):
     testfunc = globals()[fname]
     return testfunc()
 
-def t1():
+def t0():
     # Normal operation
     upub.publish(1)
+    cpub.publish(1)
+    gpub.publish(1.0)
+
+def t1():
+    # Current location
+    upub.publish(0)
     cpub.publish(1)
     gpub.publish(1.0)
 
@@ -40,18 +46,83 @@ def t4():
     rospy.sleep(0.1)
     upub.publish(0)
 
-def mantest():
-    utype = sys.argv[1]
-    udata = sys.argv[2]
-
-
-    rospy.sleep(0.2)
-    pub.publish(data = udata)
+def t5():
+    # Repeat command while crossing
+    t1()
     rospy.sleep(0.1)
+    upub.publish(1)
 
+def t6():
+    # Go and come back
+    t1()
+    rospy.sleep(7)
+    upub.publish(0)
+
+def t7():
+    # Interrupt cross, early
+    t1()
+    rospy.sleep(0.1)
+    cpub.publish(0)
+
+def t8():
+    # Interrupt cross, island, continue
+    t1()
+    rospy.sleep(3)
+    cpub.publish(0)
+    rospy.sleep(1)
+    cpub.publish(1)
+
+def t9():
+    # Interrupt cross, island, new command, cont
+    t1()
+    rospy.sleep(3)
+    cpub.publish(0)
+    upub.publish(0)
+    rospy.sleep(1)
+    cpub.publish(1)
+
+def t10():
+    # Interrupt cross, island, repeat command
+    t1()
+    rospy.sleep(3)
+    cpub.publish(0)
+    upub.publish(1)
+    rospy.sleep(1)
+    cpub.publish(1)
+
+def t11():
+    # Interrupt cross, island, no gaze
+    t1()
+    rospy.sleep(3)
+    cpub.publish(0)
+    rospy.sleep(0.5)
+    gpub.publish(0.3)
+    rospy.sleep(1)
+    cpub.publish(1)
+
+def t12():
+    # Interrupt cross, try to keep on road.
+    t1()
+    rospy.sleep(1.5)
+    cpub.publish(0)
+    rospy.sleep(0.1)
+    upub.publish(1)
+    upub.publish(0)
+    upub.publish(1)
+    upub.publish(0)
+
+def t13():
+    # Road wobble.
+    t0()
+    rospy.sleep(4.0)
+    t1()
+    rospy.sleep(3.5)
+    t0()
+    rospy.sleep(3.5)
+    t1()
 
 if len(sys.argv) < 2:
-    print "Need to specify a test number."
+    print max(int(t[1:]) for t in globals() if t.startswith('t'))
     sys.exit(1)
 else:
     rospy.init_node('manual_tests')
---
+++ src/wheely_sim/run_directed.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -x
+
+# Hide debugging output from the ROS environment files
+set +x
+
+source /opt/ros/indigo/setup.bash
+source ~/catkin_ws_3/devel/setup.bash
+
+set -x
+
+roscd wheely_sim
+(roscore & echo $! >> /tmp/mainpids) &
+# Should run stage here but CrossRoadServer needs improvement
+
+# Get number of tests
+TESTCNT=$(rosrun wheely_sim manual_tests.py)
+
+COUNTER=0
+while [ $COUNTER -le $TESTCNT ]; do
+        echo "Starting run #$COUNTER"
+	sleep 7
+	rm -f /tmp/rospids
+	# Need to set parameters
+	rosrun wheely_sim set_test_params.py
+	# Run simulator
+	(rosrun stage_ros stageros road-crossing-island.world & echo $! >> /tmp/rospids) &
+	# Run functional coverage collector
+	(rosrun wheely_sim functional_cov_collector.py & echo $! >> /tmp/rospids; mv .fcov .fcov_$COUNTER) &
+	# Run robot code
+	(COVERAGE_FILE=.coverage_crs_$COUNTER rosrun wheely_sim CrossRoadServer.py & echo $! >> /tmp/rospids) &
+	(SSCOV_FILE=.scov_$COUNTER COVERAGE_FILE=.coverage_wly_$COUNTER rosrun wheely_sim wheely.py & echo $! >> /tmp/rospids) &
+	# Run the assertion monitors TODO: SUPPRESS .scov CREATION!
+	(MONITORLOG=.monitor1_$COUNTER rosrun wheely_sim monitor1.py & echo $! >> /tmp/rospids) &
+	(MONITORLOG=.monitor2_$COUNTER rosrun wheely_sim monitor2.py & echo $! >> /tmp/rospids) &
+	(MONITORLOG=.monitor3_$COUNTER rosrun wheely_sim monitor3.py & echo $! >> /tmp/rospids) &
+	(MONITORLOG=.monitor5_$COUNTER rosrun wheely_sim monitor5.py & echo $! >> /tmp/rospids) &
+	# Run the test drivers
+	(rosrun wheely_sim manual_tests.py $COUNTER & echo $! >> /tmp/rospids)
+
+	# Need to wait for the tests to be over, can (ab)use rostopic
+	rostopic echo -n 1 --filter='m.data ==127' /user_commands
+	
+	# Give a few seconds for proper shutdown, repeat the command just in case
+	rostopic pub -1 /user_commands std_msgs/Int8 127
+	sleep 5
+
+	# Ask nicely
+	cat /tmp/rospids | xargs kill
+	
+	sleep 5
+
+	# Ask nastily
+	cat /tmp/rospids | xargs kill -9
+
+	
+        let COUNTER=COUNTER+1 
+done
+
+cat /tmp/mainpids | xargs kill
+
+# Combine the code coverage results
+coverage combine .coverage_*
+
+# Regenerate the html output
+coverage html
+
+echo "Testbench Done"
